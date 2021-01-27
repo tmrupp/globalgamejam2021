a53fb0ec36a87623c18c7847668015f9b65c9be9
tmrupp <tmrupp@umich.edu>
2021-01-27 16:32:47 -0700

    Weird Cube spawning...

---
+++ Assets/Scripts/MoreCube.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoreCube : MonoBehaviour
+{
+    GameObject cubeGuy;
+    public static int toSpawn = 100;
+    public float spawnSpeed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start ()
+    {
+        cubeGuy = (GameObject) Resources.Load("Prefabs/CubeGuy", typeof(GameObject));
+        if (toSpawn > 0)
+        {
+            StartCoroutine(SpawnCubeCoroutine(spawnSpeed));
+            toSpawn--;
+            Debug.Log("left to spawn=" + toSpawn.ToString());
+        }
+    }
+
+    IEnumerator SpawnCubeCoroutine (float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        var cG = GameObject.Instantiate(cubeGuy);
+        cG.transform.position = transform.position + new Vector3(Random.Range(-1, 1), Random.Range(0, 3), Random.Range(-1, 1));
+    }
+}
