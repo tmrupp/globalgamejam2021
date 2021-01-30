2bdd58d390ac7d42d6613fc82b355f5bbd9a2115
nichgaun <nichgaun@umich.edu>
2021-01-30 13:40:05 -0500

    Added title scene with scene transition

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    public void AdvanceScene()
+    {
+        Debug.Log("Beginning next scene");
+        SceneManager.LoadScene(1);
+    }
+}
